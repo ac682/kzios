2acf1d48d9ca4907a1cbeb2fe078c3806be00c19
Chien Zhang <zqy0224@live.com>
2022-04-27 18:26:09 +0800

    before k210 test, kernel_memory_map should be modified

--- kernel/src/mm/mod.rs
+++ kernel/src/mm/mod.rs
@@ -10,35 +10,4 @@ pub mod paged;
 pub fn init() {
     heap_allocator::init_heap();
     paged::init();
-
-    //test();
-}
-
-#[allow(unused)]
-pub fn test() {
-    extern "C" {
-        fn skernel();
-        fn ekernel();
-    }
-    struct MemoryMap {
-        kernel: (usize, usize),
-        user: (usize, usize),
-    }
-    let map = MemoryMap {
-        kernel: (skernel as usize, ekernel as usize - skernel as usize),
-        user: (ekernel as usize, MEMORY_END - ekernel as usize),
-    };
-    println!("kernel starts at {:#x}, takes {:#x} bytes, \nuser space starts at {:#x}, {:#x} bytes available", map.kernel.0, map.kernel.1, map.user.0, map.user.1);
-
-    let mut frames = Vec::<PhysicalPageNumber>::new();
-    for i in 0..256 {
-        frames.push(frame_alloc().unwrap());
-    }
-    for i in (0..256).rev() {
-        if i % 7 == 0 {
-            let frame = frames.remove(i);
-            frame_dealloc(frame)
-        }
-    }
-    print_frame_use();
 }
--- kernel/src/mm/paged/mod.rs
+++ kernel/src/mm/paged/mod.rs
@@ -17,10 +17,13 @@ pub mod table;
 pub fn init() {
     let frame = frame_alloc().unwrap();
     let kernel_table = PageTable::new(2, frame);
+    
     // 只有内核态的页表需要等值映射用于驱动
     //TODO: 地址应该从设备树获得
     // 直接全部映射！ U X R V 都是1, X 其实应该单独设置
     // 就映射内核地址，不然不够用啦
+    
+    // 这里有问题，内核在qemu中是这样的，但k210又不一样
     map(
         &kernel_table,
         VirtualAddress::from(0x80200000u64),
